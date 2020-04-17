aa02248452bbaa74d247f5117dee97ac798a9f97
Christopher Clark <christopher.w.clark@gmail.com>
2020-04-17 16:24:24 -0700

    initial population of the tools/launch directory with skeleton
    S-o-b: x@sl

---
+++ tools/launch/lcm-tool.c
@@ -0,0 +1,14 @@
+/*
+ * lcm-tool : Launch Control Module tool
+ *
+ * Tool to generate a Launch Control Module for Xen.
+ */
+
+#include <stdio.h>
+
+int main(int argc, char **argv)
+{
+    printf("lcm-tool\n");
+
+    return 0;
+}
