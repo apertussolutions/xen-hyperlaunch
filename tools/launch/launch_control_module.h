383c007cfc8460f341371c8be9cef31b813e7261
Christopher Clark <christopher.w.clark@gmail.com>
2020-04-28 08:47:48 -0700

    HEADER CHANGE

--- tools/launch/launch_control_module.h
+++ tools/launch/launch_control_module.h
@@ -56,7 +56,8 @@ struct lcm_domain_high_priv_config {
 struct lcm_domain_extended_config {
     /*
      * The length of this string is determined by the len field of the
-     * lcm_module struct, minus all fixed-length fields in lcm_module.
+     * lcm_data struct, minus all fixed-length fields in lcm_data and
+     * lcm_domain.
      */
     uint8_t config_string[0];
 };
@@ -83,42 +84,78 @@ struct lcm_xsm_flask_policy {
     uint8_t version;
 };
 
-struct lcm_module {
+struct lcm_domain_multiboot_modules {
+    /* Index of the domain kernel in the multiboot module array */
+    uint8_t kernel_index;
+    /* Boolean indicator of ramdisk presence */
+    uint8_t has_ramdisk;
+    /* Index of the domain ramdisk in the multiboot module array */
+    uint8_t ramdisk_index;
+    uint8_t pad;
+}
+
+struct lcm_domain {
     /* Type of data in this struct describing a multiboot module */
     uint16_t type;
-#define LCM_MODULE_IGNORE                   0 /* Skip this data */
-#define LCM_MODULE_LAUNCH_CONTROL_MODULE    1
-#define LCM_MODULE_DOMAIN_BASIC_CONFIG      2
-#define LCM_MODULE_DOMAIN_HIGH_PRIV_CONFIG  3
-#define LCM_MODULE_DOMAIN_EXTENDED_CONFIG   4
-#define LCM_MODULE_DOMAIN_RAMDISK           5
-#define LCM_MODULE_CPU_MICROCODE            6
-#define LCM_MODULE_XSM_FLASK_POLICY         7
-
-    /* Length of this lcm_module struct including the subtype union */
-    uint32_t len;
-
-    /* Index of this module in the multiboot module array */
-    uint8_t mb_index;
-
-    /* Padding to ensure that the union field is 4-byte aligned */
-    uint8_t pad[1];
+#define LCM_DOMAIN_DOMAIN_MODULES           1
+#define LCM_DOMAIN_DOMAIN_BASIC_CONFIG      2
+#define LCM_DOMAIN_DOMAIN_HIGH_PRIV_CONFIG  3
+#define LCM_DOMAIN_DOMAIN_EXTENDED_CONFIG   4
 
     /* Module-type-specific module data */
     union {
         uint8_t raw[0];
+        struct lcm_domain_multiboot_modules multiboot_modules;
         struct lcm_domain_basic_config basic_config;
         struct lcm_domain_high_priv_config high_priv_config;
         struct lcm_domain_extended_config extended_config;
-        struct lcm_ramdisk ramdisk;
-        struct lcm_microcode microcode;
-        struct lcm_xsm_flask_policy xsm_flask_policy;
     };
+};
+
+/* Structure containing an array declaring the type of each multiboot module */
+struct lcm_module_types {
+    uint8_t num_modules;
+
+    /* Array of: type of each multiboot module */
+    uint8_t type[0];
+#define LCM_MODULE_IGNORE                   0
+#define LCM_MODULE_LAUNCH_CONTROL_MODULE    1
+#define LCM_MODULE_DOMAIN_KERNEL            2
+#define LCM_MODULE_DOMAIN_RAMDISK           3
+#define LCM_MODULE_CPU_MICROCODE            4
+#define LCM_MODULE_XSM_FLASK_POLICY         5
+};
+
+/* Structure containing hash checksum of a multiboot module */
+struct lcm_module_checksum {
+    uint8_t module_index;
+    uint8_t algorithm;
+/* TODO: declare algorithm identifier enumeration values here */
+    uint16_t hash_len;
+    uint8_t bytes[0];
+}
+
+struct lcm_data {
+    uint32_t type;
+#define LCM_DATA_IGNORE             0 /* Skip this data */
+#define LCM_DATA_MODULE_TYPES       1 /* Declare multiboot module types */
+#define LCM_DATA_DOMAIN             2 /* Defintion for an initial domain */
+#define LCM_DATA_MODULE_CHECKSUM    2 /* Checksum of a multiboot module */
+
+    /* Length of this lcm_data struct including the subtype union */
+    uint32_t len;
+
+    union {
+        uint8_t raw[0];
+        struct lcm_module_types module_types;
+        struct lcm_domain domain;
+        struct lcm_module_checksum checksum;
+    }
 };
 
 /* File format */
 struct lcm_header_info {
     uint32_t magic_number;
 #define LCM_HEADER_MAGIC_NUMBER 0x4d434c78 /* xLCM */
-    struct lcm_module modules[0];
+    struct lcm_data data[0];
 };
