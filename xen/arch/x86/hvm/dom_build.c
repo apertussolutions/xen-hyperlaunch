14486ca634aa5148d55fa4aea695ac3d2b904536
Christopher Clark <christopher.w.clark@gmail.com>
2020-05-23 17:14:43 -0700

    implement e820 map generation

--- xen/arch/x86/hvm/dom_build.c
+++ xen/arch/x86/hvm/dom_build.c
@@ -23,6 +23,7 @@
 #include <xen/softirq.h>
 
 #include <public/launch_control_module.h>
+#include <public/hvm/e820.h>
 
 #include <asm/dom_build.h>
 #include <asm/dom0_build.h> /* FIXME: for dom0_paging_pages */
@@ -63,10 +64,101 @@ static void __init boot_domain_setup_e820(struct domain *d,
                                           unsigned long nr_pages)
 {
     /*
-     * The boot domain does not have hardware access, so a simple e820 map
-     * should suffice.
+     * The boot domain does not have hardware access, so a simple fixed
+     * e820 map should suffice.
      */
-    /* FIXME: implement this */
+    /*
+     * References:
+     * - dmesg and look for e820 examples
+     * - xen/arch/x86/hvm/dom0_build.c
+     * - tools/libxl/libxl_x86.c
+     * - firmware/hvmloader/e820.c
+     */
+    /* TODO: revisit all values and calculations here */
+
+    const uint32_t lowmem_reserved_base = 0x9e000; /* TODO: check this */
+    const uint32_t bios_image_base = 0xf0000; /* TODO: check this */
+
+    unsigned long low_pages, ext_pages, max_ext_pages, high_pages;
+    unsigned long cur_pages = 0;
+    unsigned nr = 0, e820_entries = 5;
+
+    /* low pages: below 1MB */
+    low_pages = lowmem_reserved_base >> PAGE_SHIFT;
+    if ( low_pages > nr_pages )
+        panic("Insufficient memory assigned to the boot domain\n");
+
+    /* max_ext_pages: maximum size of extended memory range */
+    max_ext_pages = (HVM_BELOW_4G_MMIO_START - MB(1)) >> PAGE_SHIFT;
+
+    /* ext pages: from 1MB to mmio hole */
+    ext_pages = nr_pages - low_pages;
+    if ( ext_pages > max_ext_pages )
+        ext_pages = max_ext_pages;
+
+    /* high pages: above 4GB */
+    high_pages = 0;
+    if ( nr_pages > (low_pages + ext_pages) )
+        high_pages = nr_pages - (low_pages + ext_pages);
+
+    /* If we should have a highmem range, add one more e820 entry */
+    if ( high_pages )
+        e820_entries++;
+
+    ASSERT(e820_entries < E820MAX);
+
+    d->arch.e820 = xzalloc_array(struct e820entry, e820_entries);
+    if ( !d->arch.e820 )
+        panic("Unable to allocate memory for boot domain e820 map\n");
+
+    /* usable: Low memory */
+    d->arch.e820[nr].addr = 0x000000;
+    d->arch.e820[nr].size = lowmem_reserved_base;
+    d->arch.e820[nr].type = E820_RAM;
+    cur_pages += (d->arch.e820[nr].size) >> PAGE_SHIFT;
+    nr++;
+
+    /* reserved: lowmem_reserved_base-0xA0000: BIOS implementation */
+    d->arch.e820[nr].addr = lowmem_reserved_base;
+    d->arch.e820[nr].size = 0xA0000 - lowmem_reserved_base;
+    d->arch.e820[nr].type = E820_RESERVED;
+    nr++;
+
+    /* gap from 0xA0000 to bios_image_base */
+
+    /* reserved: BIOS region */
+    d->arch.e820[nr].addr = bios_image_base;
+    d->arch.e820[nr].size = 0x100000 - bios_image_base;
+    d->arch.e820[nr].type = E820_RESERVED;
+    nr++;
+
+    /* usable: extended memory from 1MB */
+    d->arch.e820[nr].addr = 0x100000;
+    d->arch.e820[nr].size = ext_pages << PAGE_SHIFT;
+    d->arch.e820[nr].type = E820_RAM;
+    cur_pages += (d->arch.e820[nr].size) >> PAGE_SHIFT;
+    nr++;
+
+    /* reserved: mmio range, up to 4G */
+    /* TODO: check: is one large entry right here? examples seem to have two */
+    /* TODO: check: is blocking this entire range correct, or are gaps needed? */
+    d->arch.e820[nr].addr = HVM_BELOW_4G_MMIO_START;
+    d->arch.e820[nr].size = HVM_BELOW_4G_MMIO_LENGTH;
+    d->arch.e820[nr].type = E820_RESERVED;
+    nr++;
+
+    /* usable: highmem */
+    if ( high_pages )
+    {
+        d->arch.e820[nr].addr = 0x100000000;
+        d->arch.e820[nr].size = high_pages << PAGE_SHIFT;
+        d->arch.e820[nr].type = E820_RAM;
+        cur_pages += (d->arch.e820[nr].size) >> PAGE_SHIFT;
+        nr++;
+    }
+
+    ASSERT(nr == e820_entries);
+    ASSERT(cur_pages == nr_pages);
 }
 
 static void __init boot_domain_init_p2m(struct domain *d,
