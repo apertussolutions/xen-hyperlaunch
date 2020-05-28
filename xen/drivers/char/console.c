d5dcff5b4db1595ed9545f88e8911baacbbf9af7
Christopher Clark <christopher.w.clark@gmail.com>
2020-05-28 11:59:08 -0700

    console and hardware domain handling

--- xen/drivers/char/console.c
+++ xen/drivers/char/console.c
@@ -415,6 +415,7 @@ struct domain *console_input_domain(void)
 
 static void switch_serial_input(void)
 {
+    /*
     if ( console_rx == max_init_domid + 1 )
     {
         console_rx = 0;
@@ -430,6 +431,24 @@ static void switch_serial_input(void)
         printk(" (type 'CTRL-%c' three times to switch input)",
                opt_conswitch[0]);
     printk("\n");
+    */
+
+    /* Rotate between Xen, the boot domain and hardware domain */
+    if ( console_rx == DOMID_BOOT_DOMAIN + 1 )
+    {
+        console_rx = hardware_domain->domain_id + 1;
+        printk("*** Serial input to the hardware domain");
+    }
+    else if ( console_rx == 0 )
+    {
+        console_rx = DOMID_BOOT_DOMAIN + 1;
+        printk("*** Serial input to the boot domain");
+    }
+    else
+    {
+        console_rx = 0;
+        printk("*** Serial input to Xen");
+    }
 }
 
 static void __serial_rx(char c, struct cpu_user_regs *regs)
@@ -997,7 +1016,7 @@ void __init console_init_postirq(void)
     console_init_ring();
 }
 
-void __init console_endboot(void)
+void __init console_endboot(domid_t console_domid)
 {
     printk("Std. Loglevel: %s", loglvl_str(xenlog_lower_thresh));
     if ( xenlog_upper_thresh != xenlog_lower_thresh )
@@ -1013,11 +1032,12 @@ void __init console_endboot(void)
 
     /*
      * If user specifies so, we fool the switch routine to redirect input
-     * straight back to Xen. I use this convoluted method so we still print
-     * a useful 'how to switch' message.
+     * straight back to Xen.
      */
     if ( opt_conswitch[1] == 'x' )
-        console_rx = max_init_domid + 1;
+        console_rx = 0;
+    else
+        console_rx = console_domid + 1;
 
     register_keyhandler('w', dump_console_ring_key,
                         "synchronously dump console ring buffer (dmesg)", 0);
@@ -1028,8 +1048,20 @@ void __init console_endboot(void)
     register_irq_keyhandler('G', &do_toggle_guest,
                             "toggle host/guest log level adjustment", 0);
 
-    /* Serial input is directed to DOM0 by default. */
-    switch_serial_input();
+    if ( switch_code )
+        printk(" (type 'CTRL-%c' three times to switch input)",
+               opt_conswitch[0]);
+    printk("\n");
+}
+
+void console_to_hwdom()
+{
+    if ( hardware_domain == NULL )
+        return;
+
+    printk("Switching console to the hardware domain\n");
+
+    console_rx = hardware_domain->domain_id + 1;
 }
 
 int __init console_has(const char *device)
